be3b4fb3db2b2305349489cff42d7ae76e1d27c2
Nicholas Rostov <jonathan.w.coll@gmail.comgit config --global user.email jonathan.w.coll@gmail.com>
2017-02-13 21:29:44 -0500

    .env

--- app/controllers/mortys_controller.rb
+++ app/controllers/mortys_controller.rb
@@ -9,4 +9,24 @@ class MortysController < ApplicationController
     @morty = Unirest.get("http://localhost:3000/api/v1/ricks.json").body
     render "index.html.erb"
   end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    @morty = Unirest.post("http://localhost:3000/api/v1/ricks.json", headers:{"Accept" => "application/json"}, parameters:{character: params[:character], quote: params[:quote]}).body
+    redirect_to "/morty/#{@morty["id"]}"
+  end
+
+  def update
+    @morty = Unirest.patch("http://localhost:3000/api/v1/ricks/#{params[:id]}.json", headers:{"Accept" => "application/json"}, parameters:{character: params[:character], quote: params[:quote]}).body
+    redirect_to "/morty/#{@morty["id"]}"
+  end
+
+  def destroy
+    @morty = Unirest.delete("http://localhost:3000/api/v1/ricks/#{params[:id]}.json", headers:{"Accept" => "application/json"}, parameters:{character: params[:character], quote: params[:quote]}).body
+
+    redirect_to "/morty/#{@morty["id"]}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  
-  get "morty/", to: "mortys#index"
-  get "morty/:id", to: "mortys#show"
+
+  get "/morty/new", to: "mortys#new"
+  post "/morty", to: "mortys#create"
+  get "/morty/", to: "mortys#index"
+  get "/morty/:id", to: "mortys#show"
+  get "/morty/:id", to: "mortys#edit"
+  patch "/morty/:id", to: "mortys#update"
+  delete "/morty/:id", to: "mortys#destroy"
+
+
 end
